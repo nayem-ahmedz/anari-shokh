b9f95712f9e3049a0df4eb471bb1ebc20adc9196
nayem-ahmedz <nayemahmed2n@gmail.com>
2024-09-11 21:14:14 +0600

    Changed navigation styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Nav hamburger icon
 function toggleNav(){
     let icon = document.querySelector('.hamburger');
-    let tnc = document.querySelector('.top-nav-content');
     icon.classList.toggle('h-change');
-    tnc.classList.toggle('tnc-toggle');
+    let tpc = document.querySelector('.top-nav-content');
+    tpc.classList.toggle('tnc-toggle');
 }
